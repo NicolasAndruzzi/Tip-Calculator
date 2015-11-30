afeaf204ad8eb3b5c9476f312cfc362c79066e39
Nicolas Andruzzi <NicolasAndruzzi@gmail.com>
2015-11-29 17:11:42 -0700

    all functionality completed

--- js/app.js
+++ js/app.js
@@ -2,7 +2,8 @@ document.getElementById("submitButton").addEventListener("click", calculateTip);
 
 function calculateTip(){
   var billTotal = document.getElementById("enterField").value.replace("$", "");
-  var tip = billTotal *.2;
+  var tipPercent = document.getElementById("tipAmount").value;
+  var tip = billTotal * tipPercent;
   var display = document.getElementById("results");
   display.innerHTML = "You should tip: $" + tip.toFixed(2);
 }
